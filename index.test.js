4f3280dab3a21ebce2638aeb2c9494733c5afcb7
Jaehong Kang <sinoru@me.com>
2021-03-02 14:50:13 +0900

    Add index.test.js

---
+++ index.test.js
@@ -0,0 +1,10 @@
+const process = require('process');
+const cp = require('child_process');
+const path = require('path');
+
+// shows how the runner will run a javascript action with env / stdout protocol
+test('test runs', () => {
+  process.env['INPUT_SWIFT-VERSION'] = '5.3.3';
+  const ip = path.join(__dirname, 'index.js');
+  console.log(cp.execSync(`node ${ip}`, {env: process.env}).toString());
+})
